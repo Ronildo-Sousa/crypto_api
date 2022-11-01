63419b55611f1185a5dc86e212721fc71488bdd8
Ronildo Sousa <sousaronildo400@gmail.com>
2022-11-01 17:14:36 -0300

    changing the price format

--- app/Models/CurrencyHistory.php
+++ app/Models/CurrencyHistory.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -15,5 +16,13 @@ class CurrencyHistory extends Model
     public function coin(): BelongsTo
     {
         return $this->belongsTo(Coin::class);
+    }
+
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => floor($value / 1000),
+            set: fn ($value) => ($value * 1000),
+        );
     }
 }
